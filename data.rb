b58c8b5eafd97d5aecbe33f6ee5edfd280cbaa3c
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2014-03-27 02:41:13 +0400

    Add user groups to json output

--- data.rb
+++ data.rb
@@ -2,6 +2,9 @@
 class User
   include DataMapper::Resource
 
+  has n, :group_users
+  has n, :groups, through: :group_users
+
   property :id,             Serial
   property :username,       String
   property :admin,          Boolean
@@ -14,10 +17,32 @@ class User
       id: self.id,
       username: self.username,
       admin: self.admin,
-      moderator: self.moderator
+      moderator: self.moderator,
+      groups: self.groups.map { |g| g.name }
     }.to_json
   end
 end
+
+class GroupUser
+  include DataMapper::Resource
+
+  property :id,       Serial
+  property :group_id, Integer
+  property :user_id,  Integer
+
+  belongs_to :group,  key: true
+  belongs_to :user,   key: true
+end
+
+class Group
+  include DataMapper::Resource
+
+  property :id,   Serial
+  property :name, String
+
+  has n, :users, through: :group_users
+end
+
 
 DataMapper.setup(:default, ENV['DB_PATH'])
 DataMapper.finalize
